69b0e41a80c10c6a73ac4f0bc5d102daa1dac89a
S-C-22 <106505732+S-C-22@users.noreply.github.com>
2025-01-24 14:41:20 +0530

    Add files via upload

---
+++ modules/variables.tf
@@ -0,0 +1,57 @@
+# modules/vm/variables.tf
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "virtual_network_address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "The address prefixes for the subnet"
+  type        = list(string)
+}
+
+variable "network_interface_name" {
+  description = "The name of the network interface"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The admin username for the virtual machine"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The admin password for the virtual machine"
+  type        = string
+  sensitive   = true
+}
